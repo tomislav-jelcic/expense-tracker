9be352f14e41ce84bc89947b41b892f7361f6fe4
DESKTOP-Q51H3PK\Tomislav <tomislav.jelcic@outlook.com>
2022-03-02 22:56:25 +0100

    Removes unused FE code

--- clients/web/expense-tracker/src/App.tsx
+++ clients/web/expense-tracker/src/App.tsx
@@ -1,39 +1,13 @@
 import React from "react";
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./keycloack";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./components/Nav";
-import WelcomePage from "./pages/Homepage";
-import SecuredPage from "./pages/SecuredPage";
-import PrivateRoute from "./helpers/PrivateRoute";
-import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
       <ReactKeycloakProvider authClient={keycloak}>
         <BrowserRouter>
-          <Nav />
-          <div className="grid grid-cols-12 overflow-hidden bg-white h-full pt-5">
-            <div className="col-span-2">
-              <Sidebar />
-            </div>
-            <div className="col-span-10">
-              <div className="grid grid-cols-12">
-                <Routes>
-                  <Route path="/" element={<WelcomePage />} />
-                  <Route
-                    path="/secured"
-                    element={
-                      <PrivateRoute>
-                        <SecuredPage />
-                      </PrivateRoute>
-                    }
-                  />
-                </Routes>
-              </div>
-            </div>
-          </div>
         </BrowserRouter>
       </ReactKeycloakProvider>
     </React.Fragment>
