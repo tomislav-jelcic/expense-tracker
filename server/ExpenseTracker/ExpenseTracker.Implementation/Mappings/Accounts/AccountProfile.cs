987331ce1791dcc7c0dbf55414be9f1023d10d00
DESKTOP-Q51H3PK\Tomislav <tomislav.jelcic@outlook.com>
2022-03-02 22:43:15 +0100

    Fixes mappings in AccountProfile

---
+++ server/ExpenseTracker/ExpenseTracker.Implementation/Mappings/Accounts/AccountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ExpenseTracker.Application.Dtos.Account;
+using ExpenseTracker.Domain.Entities;
+
+namespace ExpenseTracker.Implementation.Mappings.Accounts;
+
+public class AccountProfile : Profile
+{
+    public AccountProfile()
+    {
+        CreateMap<AccountRequestDto, Account>();
+        CreateMap<Account, AccountResponseDto>();
+    }
+}
