5788080424ea296f6dc975f61b778fd635c0b149
DESKTOP-Q51H3PK\Tomislav <tomislav.jelcic@outlook.com>
2022-03-05 20:13:43 +0100

    Uppercases componentes folder

--- clients/web/expense-tracker/src/App.tsx
+++ clients/web/expense-tracker/src/App.tsx
@@ -4,7 +4,7 @@ import { ReactKeycloakProvider } from "@react-keycloak/web";
 import { BrowserRouter } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
 import keycloak, { initOptions } from "./keycloack";
-import { Loading } from "./components/Loading";
+import { Loading } from "./Components/Loading";
 
 function App() {
   return (
