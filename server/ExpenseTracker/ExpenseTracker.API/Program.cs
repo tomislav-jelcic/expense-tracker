ba16b74a6418fe1b1852e44f963cfd921df086f7
DESKTOP-Q51H3PK\Tomislav <tomislav.jelcic@outlook.com>
2022-03-26 21:26:08 +0100

    Adds serilog and elasticsearch

--- server/ExpenseTracker/ExpenseTracker.API/Program.cs
+++ server/ExpenseTracker/ExpenseTracker.API/Program.cs
@@ -1,19 +1,31 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json");
+
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.Logger.LogError("Some random stuff");
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
